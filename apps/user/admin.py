97919e99e1c1c1f75ad555c404be51bf6a830c2c
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2026-05-06 17:31:13 -0300

    add user acess

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Adicionando os campos novos na visualização do Admin
+fields = list(UserAdmin.fieldsets)
+fields.append(
+    ("Informações Extras", {'fields': ('bio', 'data_nascimento', 'telefone')})
+)
+UserAdmin.fieldsets = tuple(fields)
+
+admin.site.register(User, UserAdmin)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,18 @@
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # Adicione campos personalizados aqui
+    # bio = models.TextField(max_length=500, blank=True)
+    # data_nascimento = models.DateField(null=True, blank=True)
+    # telefone = models.CharField(max_length=15, blank=True)
+    cpf = models.CharField(max_length=11, blank=True)
+
+    # Exemplo: mudar o campo de login para e-mail em vez de username
+    email = models.EmailField('email address', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username'] # username ainda será pedido no createsuperuser
+
+    def _str_(self):
+        return self.username
